bd45a704b72eac81bc3f0bbf9060db7f61c9dbc3
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-23 18:24:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  
+  languages.each do |style, info|
+    info.each do |lang, data|
+      new_hash[lang] = data.merge({ "style".to_sym => [style] })
+      if lang == :javascript
+        new_hash[lang][:style] << :oo
+      end
+    end
+  end  
+
+  return new_hash
 end
