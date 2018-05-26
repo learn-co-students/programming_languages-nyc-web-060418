9487c605e1387f863aa5e31a24175950504c68ec
github username <github email address>
2018-05-26 12:37:13 -0700

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,29 @@
+# Programming Languages Lab
+# Laura Nadolski
+# May 26 2018
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  style_array = []
+  type_string = ""
+
+  languages.each do |oo_or_funct, names|
+    names.each do |name, type_hash|
+      if type_hash.has_value?("interpreted")
+        type_string = "interpreted"
+      else
+        type_string = "compiled"
+      end
+
+      if name == :javascript
+        style_array = [:oo, :functional]
+      else
+        style_array = [oo_or_funct]
+      end
+
+      new_hash[name] = {type: type_string, style: style_array}
+
+    end
+  end
+  return new_hash
 end
