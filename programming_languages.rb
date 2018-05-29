03cace4d30a8fe104d18d140bf59f5ca1697b77e
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-28 23:39:40 -0400

    reformatted all languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |one_level_down|
+    one_level_down[1].each do |language|
+      if new_hash[language[0]] == nil
+        new_hash[language[0]] = language[1]
+        new_hash[language[0]][:style] = [one_level_down[0]]
+      else
+        new_hash[language[0]][:style] << one_level_down[0]
+      end
+    end
+  end
+  new_hash
 end
