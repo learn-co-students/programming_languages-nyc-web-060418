338232e3456893692ecb282d59763e317345f8af
slowbeam <sedwardscode@gmail.com>
2018-05-31 11:30:12 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, name|
+      name.each do |name, type|
+        type[:style]=[style]
+      end
+  end
+  languages[:oo][:javascript][:style].concat languages[:functional][:javascript][:style]
+  languages[:functional].delete_if{|key, value| key == :javascript }
+
+  languages.each do |style, name|
+    new_hash = new_hash.merge(name)
+  end
+  new_hash
 end
