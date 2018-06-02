9d2365cbaf4e7580f4cc260dec46feca9f4977e3
github username <github email address>
2018-06-02 16:10:34 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,8 +6,6 @@ def reformat_languages(languages)
     lang_hash.each do |language, lang_data|
       unless new_hash.include?(language)
         new_hash[language] = lang_data
-      end
-      unless new_hash[language].include?(:style)
         new_hash[language][:style] = []
       end
       new_hash[language][:style] << style
@@ -15,70 +13,3 @@ def reformat_languages(languages)
   end
   new_hash
 end
-
-=begin
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
-
-GOAL
-{
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
-
-=end
